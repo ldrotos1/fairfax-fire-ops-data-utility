c07ce7c6a87a05abdc4d1aef1c971a1fb454688b
Louis Drotos <louis.drotos@gmail.com>
2021-10-16 14:06:59 -0400

    Added project setup files

---
+++ src/main/java/com/ffx/data/DataUtility/DataUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.ffx.data.DataUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ffx/data/DataUtility/DataUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ffx.data.DataUtility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

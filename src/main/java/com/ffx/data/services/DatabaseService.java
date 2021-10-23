43a2d8f394158bafe6ffc99bc337d8bf188d63d8
Louis Drotos <louis.drotos@gmail.com>
2021-10-23 18:20:33 -0400

    Created code for creating database tables

--- src/main/java/com/ffx/data/services/DatabaseService.java
+++ src/main/java/com/ffx/data/services/DatabaseService.java
@@ -1,6 +1,8 @@
 package com.ffx.data.services;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import com.ffx.data.models.DatabaseConnectionProps;
 import com.ffx.data.utilities.DatabaseAccessHelper;
+import com.ffx.data.utilities.DatabaseTablesSqlHelper;
 
 
 @Service
@@ -16,17 +19,34 @@ public class DatabaseService {
 	@Autowired
 	private DatabaseAccessHelper databaseAccessHelper;
 	
+	@Autowired
+	private DatabaseTablesSqlHelper databaseTableSqlHelper;
+	
 	/**
 	 * Builds the PostgresSQL database
 	 * 
 	 * @param connProps The database connection properties
 	 */
 	public void buildDatabase(DatabaseConnectionProps connProps) {
-		JdbcTemplate jdbcTemplate = databaseAccessHelper.getJdbcTemplate(connProps);
 		try {
+			// Gets the database connection
+			JdbcTemplate jdbcTemplate = databaseAccessHelper.getJdbcTemplate(connProps);
+			
+			// Creates the tables
+			System.out.println("Building tables");
+			databaseTableSqlHelper.getTables().stream().forEach(table -> {
+				jdbcTemplate.execute("DROP TABLE IF EXISTS " + table + " cascade");
+				jdbcTemplate.execute(databaseTableSqlHelper.getTableCreateSql(table));
+				System.out.println("Table created - " + table);
+			});
+			
+			// Closes the database connection
 			jdbcTemplate.getDataSource().getConnection().close();
-		} catch (SQLException e) {
+			System.out.println("Database built completed");
+		} 
+		catch (SQLException e) {
 			e.printStackTrace();
+			System.exit(1);
 		}
 	}
 }
---
+++ src/main/java/com/ffx/data/utilities/DatabaseTablesSqlHelper.java
@@ -0,0 +1,79 @@
+package com.ffx.data.utilities;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DatabaseTablesSqlHelper {
+	
+	private List<String> tables = Arrays.asList("address", "facility", "station");
+	
+	public List<String> getTables() {
+		return tables;
+	}
+
+	public String getTableCreateSql(String tableName) {
+		String sql = null;
+		
+		if (tableName.equals("station")) {
+			return getStationTableSql();
+		}
+		else if (tableName.equals("facility")) {
+			return getFacilityTableSql();
+		}
+		else if (tableName.equals("address")) {
+			return getAddressTableSql();
+		}
+		return sql;
+	}
+	
+	private String getStationTableSql() {
+		return "CREATE TABLE station("
+				.concat("id integer NOT NULL, ")
+				.concat("facility_id integer NOT NULL, ")
+				.concat("station_number character varying(3) NOT NULL, ")
+				.concat("battalion character varying(3), ")
+				.concat("is_volunteer boolean NOT NULL, ")
+				.concat("density character varying(15) NOT NULL, ")
+				.concat("area_description text NOT NULL, ")
+				.concat("fire_hazard_description text NOT NULL, ")
+				.concat("non_fire_hazard_description text NOT NULL, ")
+				.concat("CONSTRAINT station_pkey PRIMARY KEY (id), ")
+				.concat("CONSTRAINT fk_facility_id FOREIGN KEY (facility_id) ")
+				.concat("REFERENCES facility (id) MATCH SIMPLE ")
+				.concat("ON UPDATE CASCADE ")
+				.concat("ON DELETE CASCADE); ")
+				.concat("CREATE INDEX idx_station_number ON station(station_number);");
+	}
+	
+	private String getFacilityTableSql() {
+		return "CREATE TABLE facility("
+				.concat("id integer NOT NULL, ")
+				.concat("facility_name character varying(75), ")
+				.concat("department character varying(75), ")
+				.concat("latitude double precision NOT NULL, ")
+				.concat("longitude double precision NOT NULL, ")
+				.concat("location geometry(Point, 4326), ")
+				.concat("phone_number character varying(12) NOT NULL, ")
+				.concat("address_id integer NOT NULL, ")
+				.concat("CONSTRAINT facility_pkey PRIMARY KEY (id), ")
+				.concat("CONSTRAINT fk_addres_id FOREIGN KEY (address_id) ")
+				.concat("REFERENCES address (id) MATCH SIMPLE ")
+				.concat("ON UPDATE CASCADE ")
+				.concat("ON DELETE CASCADE); ")
+				.concat("CREATE INDEX idx_facility_id ON facility(id);");
+	}
+	
+	private String getAddressTableSql() {
+		return "CREATE TABLE address("
+				.concat("id integer NOT NULL, ")
+				.concat("street_address character varying(150), ")
+				.concat("city character varying(150), ")
+				.concat("state character varying(15), ")
+				.concat("zip_code character varying(5), ")
+				.concat("CONSTRAINT address_pkey PRIMARY KEY (id)); "
+				.concat("CREATE INDEX idx_address_id ON address(id);"));
+	}
+}

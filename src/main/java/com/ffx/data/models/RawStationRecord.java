dfff3b92cbd040d796efbd0949195666aa86f783
Louis Drotos <louis.drotos@gmail.com>
2021-10-24 14:46:21 -0400

    Added documentation to model classs

--- src/main/java/com/ffx/data/models/RawStationRecord.java
+++ src/main/java/com/ffx/data/models/RawStationRecord.java
@@ -3,6 +3,12 @@ package com.ffx.data.models;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * An instance of this class represents a station record
+ * parsed from the stations CSV data file
+ * 
+ * @author Louis Drotos
+ */
 @Getter
 @Builder
 public class RawStationRecord {
@@ -27,6 +33,12 @@ public class RawStationRecord {
 	private String fireHazardDescription;
 	private String nonFireHazardDescription;
 	
+	/**
+	 * Generates the insert SQL statement for the station 
+	 * table for this instance
+	 * 
+	 * @return The insert statement
+	 */
 	public String getStationInsertSql() {
 		return "INSERT INTO station("
 				.concat("id, facility_id, station_number, battalion, is_volunteer, fire_boxes, density, ")
@@ -44,6 +56,12 @@ public class RawStationRecord {
 				.concat("'" + nonFireHazardDescription + "')");
 	}
 	
+	/**
+	 * Generates the insert SQL statement for the facility 
+	 * table for this instance
+	 * 
+	 * @return The insert statement
+	 */
 	public String getFaclityInsertSql() {
 		return "INSERT INTO facility("
 				.concat("id, facility_name, department, latitude, longitude, location, phone_number, address_id) ")
@@ -60,6 +78,12 @@ public class RawStationRecord {
 				.concat("'" + addressId + "');");
 	}
 
+	/**
+	 * Generates the insert SQL statement for the address 
+	 * table for this instance
+	 * 
+	 * @return The insert statement
+	 */
 	public String getAddressInsertSql() {
 		return "INSERT INTO address("
 				.concat("id, street_address, city, state, zip_code) ")
